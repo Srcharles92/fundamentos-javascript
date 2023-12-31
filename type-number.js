ed99ed86407bc73a191a3e72b5b5cd5e58dc5baf
Charles Guerra <charles.guerra56@outlook.com>
2023-12-31 18:08:17 -0300

    exmplos da aula1

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// número inteiro
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZero = 0.5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number (não é um número)
+
+const Alura = 'Alura';
+console.log(Alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá Mundo!";  //aspas duplas
+const texto2 = 'Olá Mundo!';  // aspas simples
+const senha = "senhaSuperSegura123456!";
+const StringDeNumeros =  "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// template string ou template literal 
+
+const nome = `Charles`; // ela é feita com crase e serve como uma forma de manipular strings estilo o F de python
